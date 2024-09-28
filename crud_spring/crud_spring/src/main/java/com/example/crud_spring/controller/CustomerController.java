5f0ec773ae866273024a9ffd587239d8fbdac56f
Houssine2001 <Houssine.BenArous@esprit.tn>
2024-09-28 18:33:28 +0100

    second commit

--- crud_spring/crud_spring/src/main/java/com/example/crud_spring/controller/CustomerController.java
+++ crud_spring/crud_spring/src/main/java/com/example/crud_spring/controller/CustomerController.java
@@ -3,7 +3,10 @@ package com.example.crud_spring.controller;
 import com.example.crud_spring.entity.Customer;
 import com.example.crud_spring.service.CustomerService;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -18,5 +21,41 @@ public class CustomerController {
      @PostMapping("/customer")
     public Customer postCostumer(@RequestBody Customer customer) {
          return customerService.postCustomer(customer);
+     }
+
+     @GetMapping("/customers")
+     public List<Customer> getAllCustomer() {
+         return customerService.getAllCustomer();
+     }
+
+    @GetMapping("/customers/{id}")
+    public ResponseEntity<Customer> getCustomerById(@PathVariable Long id) {
+    Customer customer = customerService.getCustomerById(id);
+    if (customer == null)
+        return ResponseEntity.notFound().build();
+    return ResponseEntity.ok(customer);
+     }
+
+     @PutMapping("/customers/{id}")
+     public ResponseEntity<Customer> updateCustomer(@PathVariable Long id,@RequestBody Customer customer) {
+         Customer existingCustomer = customerService.getCustomerById(id);
+         if (existingCustomer == null)
+             return ResponseEntity.notFound().build();
+         existingCustomer.setName(customer.getName());
+         existingCustomer.setEmail(customer.getEmail());
+         existingCustomer.setPhone(customer.getPhone());
+
+         Customer updateCustomer = customerService.updateCustomer(existingCustomer);
+         return ResponseEntity.ok(updateCustomer);
+     }
+
+     @DeleteMapping("/customers/{id}")
+     public ResponseEntity<?> deleteCustomer(@PathVariable Long id) {
+         Customer existingCustomer = customerService.getCustomerById(id);
+         if (existingCustomer == null)
+             return ResponseEntity.notFound().build();
+         customerService.deleteCustomer(id);
+         return ResponseEntity.ok().build();
+
      }
 }
--- crud_spring/crud_spring/src/main/java/com/example/crud_spring/service/CustomerService.java
+++ crud_spring/crud_spring/src/main/java/com/example/crud_spring/service/CustomerService.java
@@ -5,6 +5,8 @@ import com.example.crud_spring.repository.CustomerRespository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class CustomerService {
@@ -13,6 +15,22 @@ public class CustomerService {
 
     public Customer postCustomer(Customer customer) {
         return customerRespository.save(customer);
+    }
+
+    public List<Customer> getAllCustomer() {
+         return customerRespository.findAll();
+    }
+
+    public Customer getCustomerById(Long id) {
+        return customerRespository.findById(id).orElse(null);
+    }
+
+    public Customer updateCustomer(Customer customer) {
+        return customerRespository.save(customer);
+    }
+
+    public void deleteCustomer(Long id) {
+        customerRespository.deleteById(id);
     }
 
 }
